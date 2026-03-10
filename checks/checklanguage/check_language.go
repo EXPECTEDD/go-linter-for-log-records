0485bb19af72185a9919f9214f9715b21115db4c
ashlyaud <maksimknutov90@gmail.com>
2026-03-10 17:45:11 +0300

    added analyzer for check language

---
+++ checks/checklanguage/check_language.go
@@ -0,0 +1,58 @@
+package checklanguage
+
+import (
+	"go/ast"
+	"linter/checks"
+	"slices"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "checkLanguage",
+	Doc:  "report if the language is not eng",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, f := range pass.Files {
+		ast.Inspect(f, func(n ast.Node) bool {
+			call, ok := n.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+
+			selector, ok := call.Fun.(*ast.SelectorExpr)
+			if !ok {
+				return true
+			}
+
+			pack, ok := selector.X.(*ast.Ident)
+			if !ok {
+				return true
+			}
+
+			if pack.Name == checks.ExpPackageName && slices.Contains(checks.ExpLevelsNames, selector.Sel.Name) {
+				text := call.Args[0].(*ast.BasicLit)
+				if len(text.Value) > 2 {
+					for _, r := range text.Value[1 : len(text.Value)-2] {
+						if !checkIsEng(r) {
+							pass.Reportf(call.Pos(), "the log must contain only English characters - %s", text.Value)
+							return true
+						}
+					}
+				}
+			}
+
+			return true
+		})
+	}
+	return nil, nil
+}
+
+func checkIsEng(r rune) bool {
+	if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') {
+		return true
+	}
+	return false
+}
