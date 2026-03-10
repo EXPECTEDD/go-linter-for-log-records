cb0a3e9ccd429008c69352e15760dd9db3289a9c
ashlyaud <maksimknutov90@gmail.com>
2026-03-10 20:13:14 +0300

    added sensetive data

--- checks/checkData.go
+++ checks/checkData.go
@@ -1,6 +1,7 @@
 package checks
 
 var (
-	ExpPackageName = "log"
-	ExpLevelsNames = []string{"Info", "Error", "Warn", "Debug"}
+	ExpPackageName    = "log"
+	ExpLevelsNames    = []string{"Info", "Error", "Warn", "Debug"}
+	ListSensetiveData = []string{"token", "api_key", "password"}
 )
