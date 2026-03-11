526902bac9e1c9f84033f5c2717fee4686cd3621
ashlyaud <maksimknutov90@gmail.com>
2026-03-11 03:54:13 +0300

    added implementation of the main function

--- cmd/linter/main.go
+++ cmd/linter/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"linter/checks/checkislower"
+	"linter/checks/checklanguage"
+	"linter/checks/checksensitivedata"
+	"linter/checks/checkspecialcharactersoremoji"
+
+	"golang.org/x/tools/go/analysis/multichecker"
+)
+
 func main() {
-	
+	multichecker.Main(
+		checkislower.Analyzer,
+		checklanguage.Analyzer,
+		checkspecialcharactersoremoji.Analyzer,
+		checksensitivedata.Analyzer,
+	)
 }
